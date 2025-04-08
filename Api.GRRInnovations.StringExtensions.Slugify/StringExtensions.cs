e1a5c97583368b1403a1344c9369363177957a70
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2025-04-08 00:28:59 -0300

    started package github

---
+++ Api.GRRInnovations.StringExtensions.Slugify/StringExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Api.GRRInnovations.Wrapper.Package
+{
+    public static partial class StringExtensions
+    {
+        public static string ToSlug(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return string.Empty;
+            }
+
+            value = value.ToLowerInvariant();
+
+            var builder = new StringBuilder();
+
+            foreach (char c in value)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                    builder.Append(c);
+            }
+
+            var result = builder.ToString().Normalize(NormalizationForm.FormC);
+            result = result.ToLowerInvariant();
+            result = InvalidCharacterRegex().Replace(result, ""); // remove caracteres inválidos
+            result = WhitespaceRegex().Replace(result, "-");         // substitui espaços por hífen
+            result = DuplicateHyphenRegex().Replace(result, "-");          // remove hífens duplicados
+            return result.Trim('-');
+        }
+
+        [GeneratedRegex(@"\s+")]
+        private static partial Regex WhitespaceRegex();
+
+        [GeneratedRegex(@"[^a-z0-9\s-]")]
+        private static partial Regex InvalidCharacterRegex();
+
+        [GeneratedRegex(@"-+")]
+        private static partial Regex DuplicateHyphenRegex();
+    }
+}
